bc31343941dd9373cf969bfdd33d7f0c43c23500
Patrick Epting <patrick.epting@hs-furtwangen.de>
2014-05-13 16:21:10 +0200

    Util.istErstesHalbjahr fixed

--- src/main/java/eptingpa/Util.java
+++ src/main/java/eptingpa/Util.java
@@ -4,7 +4,7 @@ public class Util {
 
 	public static boolean istErstesHalbjahr(int monat){
 		if ((monat < 1) || (monat > 12)) throw new IllegalArgumentException();
-		if(monat <= 7) return true;
+		if(monat < 7) return true;
 		return false;
 	}
 
