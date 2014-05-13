f55e420ed11785ddf5dfb29f635b59b374c3c507
Patrick Epting <patrick.epting@hs-furtwangen.de>
2014-05-13 16:09:12 +0200

    Test for multiple months added

--- src/test/java/eptingpa/UtilTest.java
+++ src/test/java/eptingpa/UtilTest.java
@@ -7,8 +7,11 @@ import junit.framework.TestSuite;
 public class UtilTest extends TestCase {
 
 	public void testistErstesHalbjahr() {
-		final int eingabe = 5;
-		assertTrue(Util.istErstesHalbjahr(eingabe));
+		final int[] eingabeArray = {-1, 0, 1, 6, 7, 11, 12, 13};
+		for(int i = 0; i <= eingabeArray.length; ++i)
+		{
+		assertTrue(Util.istErstesHalbjahr(eingabeArray[i]));
+	    }
 	}
 	
 }
