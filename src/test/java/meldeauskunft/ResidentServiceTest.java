bb460fa24fcb6ec8862dfecbe1821ac6d719fc5c
Patrick Epting <patrick.epting@hs-furtwangen.de>
2014-06-03 15:44:13 +0200

    neuester stand

--- src/test/java/meldeauskunft/ResidentServiceTest.java
+++ src/test/java/meldeauskunft/ResidentServiceTest.java
@@ -7,23 +7,24 @@ import java.util.List;
 
 import meldeauskunft.domain.Resident;
 import meldeauskunft.service.BaseResidentService;
+import meldeauskunft.service.ResidentServiceException;
 
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class ResidentServiceTest {
 	
 	ResidentRepositoryStub stub = new ResidentRepositoryStub();
+	Resident Hugo = new Resident("Hugo", "Mann", "Schillerstr.", "Stadtstadt", new Date(1990,12,31));
+	Resident Kara = new Resident("Kara", "Bauer", "Hillerstr.", "Stadtstadt", new Date(1990, 11, 20));
+	Resident Karl = new Resident("Karl", "Frau", "Chillerstr.", "Stadtstadt", new Date(1990, 11, 20));
 	
 	@Before
 	public void Initialize()
 	{
-		Resident Hugo = new Resident("Hugo", "Mann", "Schillerstr.", "Stadtstadt", new Date(1990,12,31));
-		Resident Egon = new Resident("Kara", "Bauer", "Hillerstr.", "Stadtstadt", new Date(1990, 11, 20));
-		Resident Karl = new Resident("Karl", "Frau", "Chillerstr.", "Stadtstadt", new Date(1990, 11, 20));
-		
 		stub.AddResident(Hugo);
-		stub.AddResident(Egon);
+		stub.AddResident(Kara);
 		stub.AddResident(Karl);
 	}
 	
@@ -37,4 +38,19 @@ public class ResidentServiceTest {
 		assertEquals(2, filteredResidents.size());
 	}
 	
+	@Test
+	public void testgetUniqueResident()
+	{
+		BaseResidentService BRS = new BaseResidentService();
+		BRS.setResidentRepository(stub);
+		try
+		{
+		Resident unique = BRS.getUniqueResident(Hugo);
+		assertEquals(Hugo, unique);
+		}
+        catch (ResidentServiceException e)
+        {
+        	assert(false);
+        }
+	}
 }
